a04f8e2cb7bc3cbc3b313370a44ee5c5e2b05716
Darian Madere <dalayne95@gmail.com>
2020-03-05 10:20:39 -0500

    add function for creating confusion matrixgit add functions.py

---
+++ functions.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+import pandas as pd
+import glob
+import  PIL
+from PIL import Image
+import cv2
+import time
+import matplotlib.pyplot as plt
+import scipy
+from scipy import ndimage
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import itertools
+
+
+# lets look at some image stats for each image to get min and max size, height, width
+
+def get_img_stats(directory):
+    image_list = []
+    for filename in glob.glob(directory): 
+        im=Image.open(filename)
+        image_list.append(im)
+    print("images appended!")
+    
+    dimensions = []
+    for image in image_list:
+        dimensions.append(image.size)
+    print(min(dimensions))
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
